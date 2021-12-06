9ba471082d41d2185501d756d485467aae1e37fa
YasinAhmet <77017655+YasinAhmet@users.noreply.github.com>
2021-12-06 03:47:06 +0300

    Delete Game directory

--- Game/Kaynak Kodu/Timer.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.ActionListener;
-
-public class Timer extends javax.swing.Timer {
-
-
-    public Timer(int delay, ActionListener listener) {
-        super(delay, listener);
-    }
-}
